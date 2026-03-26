bece9d427ec999b97b86e8622fab83bf211b2163
yanaveden <yanavedenova@gmail.com>
2026-03-26 17:58:43 +0300

    feat: add new cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:8000",
+        "http://176.12.66.207:5555",
     ],
     allow_credentials=True,
     allow_methods=["*"],
